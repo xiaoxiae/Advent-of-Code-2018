bd28d3e02d0be0fcafbd7d0bc7e6c888d605eb9f
xiaoxiae <tomas.slama.131@gmail.com>
2018-12-11 19:22:14 +0100

    add solution to  (the first part of) 11
    Add the "stupid" version of how to get the result.
    
    Although the problem can be solved using this version by brute-forcing the first few n x n grids, it is quite ugly and pretty much the same as this one, so I'll add it here when I've got a better one.

---
+++ 11/11-1.py
@@ -0,0 +1,30 @@
+from re import findall
+
+input = open("11.in", "r")
+serial = int(input.read().splitlines()[0])
+
+board = [[0] * 300 for _y in range(300)]
+
+# calculate all of the power levels
+for y in range(300):
+    for x in range(300):
+        powerLevel = int(str(((x + 10) * y + serial) * (x + 10))[-3]) - 5
+        board[y][x] = powerLevel
+
+# calculate sums of all 3x3 parts of the grid
+maxSum, maxSumCoords = 0, ()
+for y in range(298):
+    for x in range(298):
+        sum = 0
+
+        # find the sum of the 3x3
+        for i in range(3):
+            for j in range(3):
+                sum += board[y + i][x + j]
+
+        # if sum is larger than the current largest
+        if maxSum < sum:
+            maxSum = sum
+            maxSumCoords = (x, y)
+
+print(str(maxSumCoords[0])+","+str(maxSumCoords[1]))
